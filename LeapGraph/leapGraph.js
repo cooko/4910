71c1c4af587052de700c674be990508034eaeebd
cooko <cooko@uoguelph.ca>
2014-02-27 13:44:18 -0500

    yep updated stuff
    added grid helpers

--- LeapGraph/leapGraph.js
+++ LeapGraph/leapGraph.js
@@ -1,16 +1,17 @@
 function init() {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
   renderer = new THREE.WebGLRenderer();
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  camera.position.set(150, 150, 150);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 }
 function render() {
   requestAnimationFrame(render);
-
-  grid.draw();
-
   renderer.render(scene, camera);
 }
 function grid(length, width, height){
@@ -25,6 +26,9 @@ function grid(length, width, height){
   this.xplane.position = new THREE.Vector3( 0, 0, 0 );
   this.yplane.position = new THREE.Vector3( 0, 0, 0 );
   this.zplane.position = new THREE.Vector3( 0, 0, 0 );
+
+  this.xplane.rotation.x = Math.PI/2;
+  this.yplane.rotation.z = Math.PI/2;
 
   this.draw = draw;
   function draw(){
